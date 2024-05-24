eefcdf780e682d3022c13c8b9597c9dfb5ba3942
cristinuca <162121822+cristinuca@users.noreply.github.com>
2024-05-24 14:23:38 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,95 @@
+<?php require_once 'log/header.php'?>
+
+<body style="background-image: url('img/.jpg'); background-repeat: no-repeat; background-size: cover;">
+
+<div class="titulo">
+<h6 style="background-color: rgba(123, 250, 153, 0.718); text-align:center; padding: .4rem; width: auto; height: auto; font-size: 100px;">CRYSTYC</h6>
+<h2 style="text-align: center;">Aprende con nosotros, pendejo</h2>
+</div>
+
+<footer class="text-center" style="position: absolute; bottom: 0; background-color: rgba(red, green, blue, alpha); width: 100%;">
+                <!-- Grid container -->
+                <div class="container p-4 pb-0">
+                  <!-- Section: Social media -->
+                  <section class="mb-4">
+                    
+                    <!-- Facebook -->
+                    <a
+                    data-mdb-ripple-init
+                      class="btn text-white btn-floating m-1"
+                      style="background-color: #3b5998; border-radius: 50px;"
+                      href="https://www.facebook.com/"
+                      role="button"
+                      
+                      ><i class="fab fa-facebook-f"></i
+                    >Facebook</a>
+
+
+
+                    
+
+                    <a
+                    data-mdb-ripple-init
+                    class="btn text-white btn-floating m-1"
+                    style="background-color: #55acee;"
+                    href="https://zoom.us/es/signin#/login"
+                    role="button"
+                    ><i class="fab fa-twitter"></i
+                  >Zoom</a>
+
+
+
+
+
+
+
+
+                  <a
+        data-mdb-ripple-init
+        class="btn text-white btn-floating m-1"
+        style="background-color: #dd4b39; border-radius: 50px;"
+        href="https://www.instagram.com/"
+        role="button"
+        ><i class="fab fa-google"></i
+      >Instagram</a>
+
+            
+
+
+</div>
+
+
+
+
+<div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
+  © 2020 Copyright:
+  <a class="text-body" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+</div>
+
+
+        </footer>
+
+
+</body>
+
+<style>
+
+  body{
+    min-height: 100vh;
+  }
+  
+  .titulo {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    margin: 0 auto;
+    margin-top: 50px;
+    width: 100%;
+    max-width: 600px;
+    place-content: center;
+  }
+
+
+ 
+
+</style>
